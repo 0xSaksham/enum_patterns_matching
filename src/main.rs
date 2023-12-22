10a4bfa20d081c1d590a1c759e4ef947352360d8
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-12-22 14:26:54 +0530

    Refactor code to use enum and implement methods

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,33 @@ enum IpAddrKind{
     V4,
     V6
 }
-
-struct IpAddr{
-    kind:IpAddrKind,
-    address:String,
+// Easier and better way
+enum IpAddr{
+    V4(String),
+    V6(String),
 }
 
-let home = IpAddr{
-    kind:IpAddrKind::V4,
-    address:String::from("127.0.0.1"),
-};
+// Multiple types in 1 enum possible 
+enum Message{
+    Quit,
+    Move{x:i32,y:i32},
+    Write(String),
+    ChangeColor(i32,i32,i32),
+}
 
-let loopback = IpAddr{
-    kind:IpAddrKind::V6,
-    address:String::from("::1"),
-};
+// Implementation works same for structs and enums 
+impl Message{
+    fn call(&self){
+        // method body
+    }
+}
+
+let m :String = Message::Write(String::from("Hello"));
+m.call();
+
+let home = IpAddr::V4(String::from("127.0.0.1"));
+
+let loopback = IpAddr::V6(String::from("::1"));
 fn main() {
     // Enum Values
     let four = IpAddrKind::V4;
@@ -25,6 +37,13 @@ fn main() {
     // Route function use 
     route(IpAddrKind::V4);
     route(IpAddrKind::V6);
+
+    // Option Enum
+
+    let some_number = Some(5);
+    let some_char = Some('s');
+
+    let absent_number :Option<i32> = None;
 }
 
 // Function to use Enum IpAddrKind
